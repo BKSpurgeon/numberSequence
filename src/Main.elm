2c20bf9290165d4b9a4c7f4a86cf6c80be368d64
BK <benkoshy@hotmail.com>
2020-04-12 18:14:01 +1000

    Add: hello world of monkey video embedded

--- src/Main.elm
+++ src/Main.elm
@@ -2,9 +2,11 @@ module Main exposing (..)
 
 import Browser
 import Browser.Navigation as Nav
-import Html exposing (Html, text, div, h1, img, section, li, a, b, ul)
-import Html.Attributes exposing (src, class, href)
+import Html exposing (Html, text, div, h1, img, section, li, a, b, ul, iframe)
+import Html.Attributes exposing (src, class, href, width, height, property)
 import Url
+
+import Json.Encode
 
 {-
 To do:
@@ -106,11 +108,23 @@ explanationOfTheGame model =
   , body =
       [ div [ class "container"]
             [ h1 [] [text "Man vs Chimp"]]     
-      , viewLink "/home" "Back to game"
+      , viewLink "/home" "Back to game"  
+      , videoframe          
       ]
   }
-
 
 viewLink : String -> String -> Html msg
 viewLink path textAnnotation =
   div [] [ a [ href path ] [ text textAnnotation ] ]
+
+videoframe =
+  iframe
+  [ width 560
+  , height 315
+  , src "https://www.youtube.com/embed/zsXP8qeFF6A"
+  , property "frameborder" (Json.Encode.string "0")
+  , property "allowfullscreen" (Json.Encode.string "true")  
+  ]
+  []
+
+{-, property "allow" (Json.Encode.string "accelerometer; gyroscope; picture-in-picture")-}
