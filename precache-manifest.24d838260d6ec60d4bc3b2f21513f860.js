baad369e8603fc3cbd22c490d063ed25d255cc02
BK <benkoshy@hotmail.com>
2020-04-12 18:24:15 +1000

    Updates

---
+++ precache-manifest.24d838260d6ec60d4bc3b2f21513f860.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4c6c26e808603d3e0765d97c07b997",
+    "url": "/numberSequence/favicon.ico"
+  },
+  {
+    "revision": "c70d7ca3ff5cfa1dcc6a61864b3fc252",
+    "url": "/numberSequence/favicon_io/android-chrome-192x192.png"
+  },
+  {
+    "revision": "9ca1f9264db5bf5d5659b9e6c6d3754f",
+    "url": "/numberSequence/favicon_io/android-chrome-512x512.png"
+  },
+  {
+    "revision": "22ab513ef03a6d9805b7ba7b97115a1f",
+    "url": "/numberSequence/favicon_io/apple-touch-icon.png"
+  },
+  {
+    "revision": "8cb1f0ac9044c3b4a9665f2616db02e7",
+    "url": "/numberSequence/favicon_io/favicon-16x16.png"
+  },
+  {
+    "revision": "d256d54d63f8d83466036cfaf0335669",
+    "url": "/numberSequence/favicon_io/favicon-32x32.png"
+  },
+  {
+    "revision": "7c4c6c26e808603d3e0765d97c07b997",
+    "url": "/numberSequence/favicon_io/favicon.ico"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/numberSequence/favicon_io/site.webmanifest"
+  },
+  {
+    "revision": "d739a23bdeefc0b1ab59d76d564d9674",
+    "url": "/numberSequence/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/numberSequence/logo.svg"
+  },
+  {
+    "revision": "0b68540534d685725d301e220751c5a3",
+    "url": "/numberSequence/manifest.json"
+  },
+  {
+    "revision": "2d7288f30b7b224a74a5",
+    "url": "/numberSequence/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "2d7288f30b7b224a74a5",
+    "url": "/numberSequence/static/js/main.2d7288f3.chunk.js"
+  },
+  {
+    "revision": "3e952f2e8e70386d81eb",
+    "url": "/numberSequence/static/js/runtime~main.3e952f2e.js"
+  },
+  {
+    "revision": "7aa09ec89c957a98c55b",
+    "url": "/numberSequence/static/js/vendors~main.7aa09ec8.chunk.js"
+  }
+]);
